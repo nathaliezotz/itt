a24dad174b381e3a81e26afd2dd64881f8047d98
Fabian Schatz <Fabian.Schatz@googlemail.com>
2017-05-22 08:30:56 +0200

    Updated Experiment

--- assignment3/reaction_time_test.py
+++ assignment3/reaction_time_test.py
@@ -35,7 +35,7 @@ O = Odd
 5xPNO j
 """
 
-
+# ADB ANB PDE PNE
 
 
 '''
@@ -51,9 +51,11 @@ Logging (writeLogToFile)
 
 '''
 
+import csv
+import os
 import random
 import sys
-
+import datetime
 from PyQt5 import QtGui, QtWidgets, QtCore
 
 
@@ -72,7 +74,7 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
         self.initConfig()
 
         self.current_task = -1
-        self.current_state = "Pause"
+        self.current_state = 'Pause'
 
         self.tasks = {'ADB': self.taskADB(),
                       'ADR': self.taskADR(),
@@ -84,11 +86,29 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
                       'PNO': self.taskPNO()}
 
         self.counter = 0
-
+        self.current_trial = None
         self.timer_draw = QtCore.QTimer(self)
         self.timer_draw.timeout.connect(self.drawDistraction)
         self.timer_draw.start(self.BLINK_SPEED)
 
+# participant ID
+# shown stimulus (concrete word/color/number/etc.),
+# mental complexity (pre-attentive vs. attentive) of stimulus
+# distraction (yes/no)
+# pressed key
+# whether the correct key was pressed
+# reaction time
+# timestamp
+# all other information you deem important
+        self.logging_dict = {'id': [],
+                             'shown_stimulus': [],
+                             'mental_complexity': [],
+                             'distraction': [],
+                             'pressed_key': [],
+                             'correct_key_pressed': [],
+                             'reaction_time_in_microseconds': [],
+                             'timestamp': [],
+                             }
 
 
     def initUI(self):
@@ -103,7 +123,7 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
             for line in config_file:
                 if line.startswith('PARTICIPANT'):
                     print(line)
-                    self.participant_id = line.split(" ")[1]
+                    self.participant_id = line.split(" ")[1].rstrip()
                 elif line.startswith('TRIALS'):
                     print(line)
                     self.trials = line.split('[')[1][:-2].replace(" ", "").replace("'", "").split(",")
@@ -115,17 +135,19 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
 
     def drawPauseScreen(self, event, qp):
         print("draw pause")
-        self.current_state = "Pause"
+        self.current_state = 'Pause'
 
-        qp.drawText(self.WIDTH/2, self.HEIGHT/2, "PAUSE \n PRESS 'SPACE' TO CONTINUE!")
-
+        qp.setFont(QtGui.QFont('Helvetica', 32))
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, "PAUSE\n\nPRESS 'SPACE' TO CONTINUE!")
+        self.update()
 
     def startNextTask(self):
-        print("start necxt task")
+        print("start next task")
         self.current_task += 1
-        self.current_state = "Trial"
+        self.current_state = 'Trial'
 
-        self.trials[self.current_task]
+        self.current_trial = self.trials[self.current_task]
+        self.task_start_time = datetime.datetime.now()
 
 
     def taskADB(self):
@@ -152,35 +174,72 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
     def taskPNO(self):
         self.update()
 
+    def handleInput(self, key):
+        self.task_end_time = datetime.datetime.now()
+        self.current_state = 'Pause'
+        self.update()
+
+        self.logging_dict['id'].append(self.participant_id)
+        self.logging_dict['shown_stimulus'].append(self.current_trial[2])
+        self.logging_dict['mental_complexity'].append(self.current_trial[0])
+        self.logging_dict['distraction'].append('yes' if self.current_trial[1] == 'D' else 'no')
+        self.timedelta = self.task_end_time - self.task_start_time
+        self.logging_dict['reaction_time_in_microseconds'].append((self.timedelta.seconds * 1000000 + self.timedelta.microseconds))
+        self.logging_dict['timestamp'].append(int(self.task_end_time.timestamp()))
+        self.logging_dict['pressed_key'].append(key.text())
+        # self.current_trial = self.trials[self.current_task]
+        if self.current_trial in ['ADB', 'ANB', 'PDE', 'PNE'] and key == QtCore.Qt.Key_F:
+            self.logging_dict['correct_key_pressed'].append('true')
+        elif self.current_trial in ['ADR', 'ANR', 'PDO', 'PNO'] and key == QtCore.Qt.Key_J:
+            self.logging_dict['correct_key_pressed'].append('true')
+        else:
+            self.logging_dict['correct_key_pressed'].append('false')
 
     def writeLogToFile(self):
-        pass
+        """
+
+        check logging indexing and maybe different method!!!
+
+
+        """
+
+
+        filepath = 'reaction_time_test_log.csv'
+        log_file_exists = os.path.isfile(filepath)
+        print(log_file_exists)
+        with open(filepath, 'a') as log_file:
+            w = csv.DictWriter(log_file, self.logging_dict.keys())
+            if not log_file_exists:
+                w.writeheader()
+
+
+            for x in range(0, len(self.logging_dict['timestamp'])):
+                current_row = ''
+                for key in self.logging_dict.keys():
+                    if x == 0:
+                        current_row += self.logging_dict[key][x]
+                    else:
+                        current_row += ',{}'.format(str(self.logging_dict[key][x]))
+
+
+            # w.writerows(self.logging_dict)
 
     def keyPressEvent(self, event):
         print(event.key())
+
+        if event.key() == QtCore.Qt.Key_S:
+            print(self.logging_dict)
+            self.writeLogToFile()
         if self.current_state == "Pause":
             if event.key() == QtCore.Qt.Key_Space:
                 self.startNextTask()
 
         elif self.current_state == "Trial":
             if event.key() == QtCore.Qt.Key_F:
-                """
-
-                handleInput
-
-
-                """
-                self.current_state = 'Pause'
-                self.update()
+                self.handleInput(QtCore.Qt.Key_F)
             elif event.key() == QtCore.Qt.Key_J:
-                """
+                self.handleInput(QtCore.Qt.Key_J)
 
-                handleInput
-
-
-                """
-                self.current_state = 'Pause'
-                self.update()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
@@ -190,12 +249,12 @@ class ReactionTimeExperiment(QtWidgets.QWidget):
         print(self.current_state)
         print(self.trials[self.current_task])
 
-
         if self.current_state == 'Pause':
             self.drawPauseScreen(event, qp)
 
         elif self.current_state == 'Trial':
-            if 'D' in self.trials[self.current_task]:
+            print(self.current_trial)
+            if 'D' in self.current_trial:
                 for x in range(self.NUM_SQUARES):
                     for y in range(self.NUM_SQUARES):
                         qp.setBrush(QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
